51678326ce1ad2405a0a176674a8fd7bed1b157e
Nikita <nikita.kras.kyiv@gmail.com>
2022-03-21 17:57:08 +0000

    The court page now has 3 differetn courts. Need to add a class Court, need to save and load Court from Database, a lot of things need to be done. The Courts must be aligned to be in a row as well

--- src/pages/Courts.js
+++ src/pages/Courts.js
@@ -35,11 +35,77 @@ class ListOfCourts extends React.Component {
 
 export default ListOfCourts  */ 
 
+import { Button } from 'bootstrap';
+import styled from 'styled-components'
+import Card from "react-bootstrap/Card";
+import React from 'react';
+
+/*
+class Court extends React.Component {
+
+	render () {
+		return (
+		<div className="col-lg-4 align-items-center d-flex">
+		  <Card style={{ width: '18rem' ,color:"#000" }}>
+			<Card.Img variant="top" src="assets/images/sp.png" />
+				<Card.Body>
+					<Card.Title>The Grip</Card.Title>
+					<Card.Text>
+					Some quick example text to build on the card title and make up the bulk of
+					the card's content.
+					</Card.Text>
+				</Card.Body>
+			</Card>
+		</div>
+		)
+	}
+};
+*/
+
 const Courts = () => {
     return (<>
       <h1>Courts</h1>
       <p>This is a Forum page.</p>
-      </>);
+      
+
+			    <div width="30%">
+				  <Card style={{ width: '18rem' ,color:"#000" }}>
+					<Card.Img variant="top" src="assets/images/sp.png" />
+						<Card.Body>
+							<Card.Title>Court one</Card.Title>
+							<Card.Text>
+							I am a description! text to build on the card title and make up the bulk of
+							the card's content.
+							</Card.Text>
+						</Card.Body>
+					</Card>
+
+				  <Card style={{ width: '18rem' ,color:"#000" }}>
+					<Card.Img variant="top" src="assets/images/sp.png" />
+						<Card.Body>
+							<Card.Title>Title two</Card.Title>
+							<Card.Text>
+							Some quick example text to build on the card title and make up the bulk of
+							the card's content.
+							</Card.Text>
+						</Card.Body>
+					</Card>
+
+				  <Card style={{ width: '18rem' ,color:"#000" }}>
+					<Card.Img variant="top" src="assets/images/sp.png" />
+						<Card.Body>
+							<Card.Title>The Grip</Card.Title>
+							<Card.Text>
+							Some quick example text to build on the card title and make up the bulk of
+							the card's content.
+							</Card.Text>
+						</Card.Body>
+					</Card>
+				</div>
+				
+	  
+      </>
+	  );
   };
   
   export default Courts;
