b60244c330540bc8e50b6d0eb0f198caa4a95eee
Bijay Shrestha <bijayshrestha@Bijays-MacBook-Air.local>
2022-04-21 10:24:53 +0100

    backend integration for triaing request

--- src/pages/TrainingRequest.js
+++ src/pages/TrainingRequest.js
@@ -7,23 +7,53 @@
 */
 
 import styled from 'styled-components'
+import {useState} from 'react'
+import { PostTrainingRequest } from '../services/PostTrainingRequest';
 const TrainingRequest = () => {
+
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [success, setSuccess] =useState()
+    const values = 
+        {
+        title:'',
+        description:''
+        }
+    
+    const handleSubmit= (e) => {
+        e.preventDefault();
+        values.title = title
+        values.description = description
+        // console.log(values)
+        PostTrainingRequest(values).then((result) => {
+            let responseJSON:any = result;
+            console.log(responseJSON);
+            if (responseJSON.status===200|| responseJSON.status===201) {
+              console.log('Sent');
+              setSuccess(true)
+            }
+            else
+            {
+                setSuccess(false)
+            }
+        })
+      }
     return (
         <div class="container">
         <h1 style={{padding: "24px 0", fontSize: "24px",fontWeight:'bolder',fontFamily:'None'}}>Request New Training Session</h1>
             <div class="row justify-content-md-center">
                 <div className='col-md' style={{maxWidth:'600px'}}>
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         {/* <!-- Name input --> */}
                         <div class="form-outline mb-4">
                         <label class="form-label" for="form4Example1">Title</label>
-                            <input type="text" id="form4Example1" class="form-control" />
+                            <input type="text" name="title" value={title} class="form-control" onChange={e => setTitle(e.target.value)} />
                         </div>
 
                         {/* <!-- Message input --> */}
                         <div class="form-outline mb-4">
                         <label class="form-label" for="form4Example3">Description</label>
-                            <textarea class="form-control" id="form4Example3" rows="4"></textarea>
+                            <textarea class="form-control" value={description} name="description" onChange={e => setDescription(e.target.value)} rows="4"></textarea>
                         </div>
 
                         {/* <!-- Submit button --> */}
